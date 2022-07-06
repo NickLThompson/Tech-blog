d2d77750effe54d832bcef81a3f31bc88f916c3b
Nickolas Thompson <thompsonnickolas1@gmail.com>
2022-07-06 14:52:19 -0400

    finalized file structure multiple times for security and began on models

---
+++ models/Comment.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model {}
+
+// Comment
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
